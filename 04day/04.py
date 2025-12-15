4ce406a5429099f45a0faf7e47566e75210dea04
Erick L <ecklpz@gmail.com>
2025-12-15 08:55:44 -0500

    4th puzzle pt1 complete

--- 04day/04.py
+++ 04day/04.py
@@ -1,18 +1,19 @@
-PATH_FILE="04_example.txt"
+PATH_FILE="04.txt"
 
-def can_be_acc( i, j, grid ):
-    grid[i-1][j-1]
-    grid[i-1][j]
-    grid[i-1][i+1]
-
-    grid[i][j-1]
-    grid[i][j]
-    grid[i][i+1]
-
-    grid[i+1][j-1]
-    grid[i+1][j]
-    grid[i+1][i+1]
-    pass
+def can_be_acc(i, j, grid):
+    counter = 0
+    # -1, 0, 1
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            if x == 0 and y == 0:
+                continue
+            ni, nj = i + x, j + y
+            # boundary check
+            if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]):
+                position = grid[ni][nj]
+                if position == "@":
+                    counter += 1
+    return counter
 
 def printing_department(path_file):
     try:
@@ -21,19 +22,17 @@ def printing_department(path_file):
         with open(path_file, 'r') as file:
             lines = file.readlines()
             grid = [list(line.strip()) for line in lines]
-            for i in range(len(grid)):
-                line = grid[i]
-                for j in range(len(line)):
-                    # i--:
-                    # i: j-- j j++
-                    # i++:
-                    can_be_acc( i, j, grid )
-                    # if can_be_access(i,j):
-                        # total_accesssible += 1
-                    pass
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '@':
+                    neighbor_count = can_be_acc(i, j, grid)
+
+                    if neighbor_count < 4:
+                        total_accesssible += 1
+        print(f"total_accesssible: {total_accesssible}")
 
     except FileNotFoundError:
-        print(f"Error: {path_file}")
-
+        print(f"Error: {path_file} not found")
 
 printing_department(PATH_FILE)
